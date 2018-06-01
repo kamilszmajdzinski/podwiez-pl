87cdc8eb86026facd134524aa88e8b2426bde316
kamilszmajdzinski <kamil.szmajdzinski@gmail.com>
2018-06-02 01:31:11 +0200

    add logout confirmation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import SignIn from './components/SignIn/index'
 import SignUp from './components/SignUp/index'
 import Landing from './components/Landing';
 import Dashboard from './components/Dashboard'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <main>
+      <MuiThemeProvider>
           <Route exact path="/" component={Landing} />
           <Route path="/signup" component={SignUp} />
           <Route path="/dashboard" component={Dashboard} />
-        </main>
-      </div>
+       </MuiThemeProvider>
     );
   }
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,20 +3,76 @@ import './style.css'
 import { logoutUser } from "../../actions/userActions";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {drawerOpen: false, dialogOpen: false};
+    }
 
     handleLogout = () => {
         this.props.logoutUser();
     }
 
+    handleDrawerToggle = () => this.setState({drawerOpen: !this.state.drawerOpen});
+    handleDrawerClose = () => this.setState({drawerOpen: false});
+    handleDialogOpen = () => {
+        this.setState({dialogOpen: true});
+      };
+    
+      handleDialogClose = () => {
+        this.setState({dialogOpen: false});
+      };
+
+
   render() {
+    const actions = [
+        <FlatButton
+          label="Anuluj"
+          primary={true}
+          onClick={this.handleDialogClose}
+        />,
+        <FlatButton
+          label="Wyloguj"
+          secondary={true}
+          onClick={this.handleLogout}
+        />,
+      ];
     return (
-      <div className = 'header'>
-        <i class="fas fa-bars" title= 'Menu'></i>
-        <img src = {require('./podwiez2.png')}></img>
-        <i class="fas fa-sign-out-alt" title= 'Wyloguj się' onClick = {this.handleLogout} ></i>
-      </div>
+        <div>
+        <div className = 'header'>
+            <i class="fas fa-bars" title= 'Menu' onClick={this.handleDrawerToggle}></i>
+            <img src = {require('./podwiez2.png')}></img>
+            <i class="fas fa-sign-out-alt" title= 'Wyloguj się' onClick = {this.handleDialogOpen} ></i>
+        </div>
+      <Drawer
+      docked={false}
+      width={200}
+      open={this.state.drawerOpen}
+      onRequestChange={(drawerOpen) => this.setState({drawerOpen})}
+    >
+      <MenuItem onClick={this.handleDrawerClose}>Menu Item</MenuItem>
+      <MenuItem onClick={this.handleDialogOpen}>Wyloguj się</MenuItem>
+    </Drawer>
+
+         
+        <Dialog
+          title = "Potwierdź wylogowanie"
+          actions={actions}
+          modal={true}
+          open={this.state.dialogOpen}
+          onRequestClose={this.handleDialogClose}
+        >
+          Czy na pewno chcesz się wylogować?
+        </Dialog>
+
+    </div>
     )
   }
 }
